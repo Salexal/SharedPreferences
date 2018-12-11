4e2bf0eabb02a501d8ec891f9cf58281a4a263e7
Administrator <s@>
2018-12-11 11:33:37 +0800

    spinner

--- app/src/main/java/com/example/administrator/testdemo/Main2Activity.java
+++ app/src/main/java/com/example/administrator/testdemo/Main2Activity.java
@@ -3,8 +3,22 @@ package com.example.administrator.testdemo;
 import android.content.SharedPreferences;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class Main2Activity extends AppCompatActivity {
+
+    private List<String> list = new ArrayList<String>();
+    private TextView myTextView;
+    private Spinner mySpinner;
+    private ArrayAdapter<String> adapter;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -14,7 +28,19 @@ public class Main2Activity extends AppCompatActivity {
         SharedPreferences.Editor editor = key.edit();
         editor.putBoolean("isVis",true);
         editor.commit();
-        
-
+        // 第一步：添加一个下拉列表项的list，这里添加的项就是下拉列表的菜单项
+        list.add("北京");
+        list.add("上海");
+        list.add("广州");
+        list.add("深圳");
+        mySpinner = (Spinner) findViewById(R.id.spinner);
+        // 第二步：为下拉列表定义一个适配器，这里就用到里前面定义的list。
+        adapter = new ArrayAdapter<String>(this,
+                android.R.layout.simple_spinner_item, list);
+        // 第三步：为适配器设置下拉列表下拉时的菜单样式。
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        // 第四步：将适配器添加到下拉列表上
+        mySpinner.setAdapter(adapter);
+        // 第五步：为下拉列表设置各种事件的响应，这个事响应菜单被选中
     }
 }
